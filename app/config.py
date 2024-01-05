7519ba8fdeab5641da8bcc0dac146c5de24f13b1
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-05 15:19:18 -0300

    fix: default config values

--- app/config.py
+++ app/config.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 
 class Config(BaseModel):
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT")
-    DB_NAME: str = os.getenv("DB_NAME")
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_NAME: str = os.getenv("DB_NAME", "mydatabase")
+    DB_USER: str = os.getenv("DB_USER", "myuser")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "mypassword")
