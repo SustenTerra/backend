6197c6baa187a32bf2779a996084a5fb6fca3044
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2024-01-05 16:09:48 -0300

    fix: change db port

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 class Config(BaseModel):
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_PORT: str = os.getenv("DB_PORT", "5433")
     DB_NAME: str = os.getenv("DB_NAME", "mydatabase")
     DB_USER: str = os.getenv("DB_USER", "myuser")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "mypassword")
