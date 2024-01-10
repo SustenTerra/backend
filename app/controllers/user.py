becee3cb03b11f24f775424b09c096c40df19298
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-10 18:20:22 -0300

    feat: add base controller

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,26 +1,17 @@
-from fastapi import HTTPException
-
+from app.controllers.base import BaseController
 from app.exceptions.user import UserAlreadyRegisteredException
 from app.models.user import User
 from app.repositories.user import UserRepository
-from app.schemas.users import UserCreate
+from app.schemas.users import UserCreate, UserUpdate
 
 
-class UserController:
-    def __init__(self, repository: UserRepository):
-        self.repository = repository
-
+class UserController(
+    BaseController[User, UserRepository, UserCreate, UserUpdate]
+):
     def create(self, create: UserCreate):
         found_user = self.repository.get_by_email(create.email)
 
         if found_user:
             raise UserAlreadyRegisteredException(email=create.email)
 
-        user = User(
-            full_name=create.full_name,
-            email=create.email,
-            password=create.password,
-        )
-        self.repository.add(user)
-
-        return user
+        return super().create(create)
