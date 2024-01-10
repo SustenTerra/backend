78a00010a6054f1441295fb68823a1b194710961
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-10 17:50:16 -0300

    fix: rename db to session

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,16 +1,10 @@
-from typing import Any, Dict, Generic, List, Optional, TypeVar
-from uuid import UUID
+from typing import Any, Generic, List, Optional, TypeVar
 
 from sqlalchemy import Column
 from sqlalchemy.orm import Query, Session
 from sqlalchemy.orm.decl_api import DeclarativeMeta
 
 T = TypeVar("T", bound=DeclarativeMeta)
-ID = TypeVar("ID")
-
-CREATE = TypeVar("CREATE")
-UPDATE = TypeVar("UPDATE")
-RETURN = TypeVar("RETURN")
 
 
 class BaseRepository(Generic[T]):
@@ -19,14 +13,14 @@ class BaseRepository(Generic[T]):
     def __init__(
         self,
         model_class: T,
-        db: Session,
+        session: Session,
     ):
         self.model_class = model_class
-        self.db = db
+        self.session = session
 
     @property
     def default_query(self) -> Query:
-        return self.db.query(self.model_class)
+        return self.session.query(self.model_class)
 
     def get(self) -> Query:
         return self.default_query
@@ -38,13 +32,13 @@ class BaseRepository(Generic[T]):
         return self.default_query.get(id)
 
     def add(self, model: T) -> T:
-        self.db.add(model)
-        self.db.commit()
+        self.session.add(model)
+        self.session.commit()
         return model
 
     def update(self, id: int, values: dict[str, Any]) -> Optional[T]:
         self.default_query.filter_by(id=id).update(values)  # type: ignore
-        self.db.commit()
+        self.session.commit()
         return self.get_by_id(id)
 
     def delete(self, id: int) -> None:
