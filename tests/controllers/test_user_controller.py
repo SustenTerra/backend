f9e37c8b4778dcb2d274cc6431ed980e14e54b56
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-11 13:48:24 -0300

    feat: add pytest controller test

---
+++ tests/controllers/test_user_controller.py
@@ -0,0 +1,29 @@
+import pytest
+
+from app.controllers.user import UserController
+from app.models.user import User
+from app.repositories.user import UserRepository
+from app.schemas.users import UserCreate
+
+
+class TestUserController:
+    @pytest.fixture
+    def setup(self, db_session):
+        self.repository = UserRepository(User, db_session)
+        self.controller = UserController(User, self.repository)
+
+    def test_create_user(self, setup):
+        create = UserCreate(
+            email="test@email.com",
+            full_name="Test User",
+            password="password",
+        )
+
+        user = self.controller.create(create)
+
+        assert user.id is not None
+        assert user.email == create.email
+        assert user.full_name == create.full_name
+
+        found_user = self.repository.get_by_id(user.id)
+        assert found_user is not None
