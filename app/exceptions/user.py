dea8bae3bc3ae006e19ba3f5dbcce28d793b7879
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-20 17:52:44 -0300

    fix: conflicts resolved

--- app/exceptions/user.py
+++ app/exceptions/user.py
@@ -8,18 +8,18 @@ class UserAlreadyRegisteredException(HTTPException):
             detail=f"Email {email} already registered",
         )
 
-<<<<<<< Updated upstream
+
 class UserPasswordDoNotMatchException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=400,
             detail=f"Given current password do not match actual user password",
-=======
+        )
+
 
 class UserNotFoundException(HTTPException):
     def __init__(self, email: str):
         super().__init__(
             status_code=404,
             detail=f"User with email {email} not found",
->>>>>>> Stashed changes
         )
