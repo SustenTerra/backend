3f1292433656797e548fb067b2dda9b3d3e0d14e
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-20 18:02:39 -0300

    feat: add sessions routes

--- app/controllers/session.py
+++ app/controllers/session.py
@@ -1,16 +1,20 @@
 from sqlalchemy.orm import Session
 
-from app.exceptions.user import UserNotFoundException
+from app.exceptions.user import UserPasswordDoNotMatchException
+from app.hashing import Hasher
 from app.models import User
 from app.repositories.user import UserRepository
 
 
 class SessionController:
-    def __init__(self, session: Session):
-        self.user_repository = UserRepository(User, session)
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
 
     def login(self, email: str, password: str):
         found_user = self.user_repository.get_by_email(email)
 
         if not found_user:
-            raise UserNotFoundException(email)
+            raise UserPasswordDoNotMatchException()
+
+        if not Hasher.verify_password(password, found_user.password):
+            raise UserPasswordDoNotMatchException()
