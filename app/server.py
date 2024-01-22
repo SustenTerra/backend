afa633e943cdedaeacbafc36da118e5f0aa5b9ac
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-22 18:32:58 -0300

    chore: remove uselesss imports on server file

--- app/server.py
+++ app/server.py
@@ -1,14 +1,10 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
-from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI
 
 from app.database.connection import engine
-from app.models import Base, User
-from app.repositories.user import UserRepository
+from app.models import Base
 from app.routes.courses import courses as courses_router
 from app.routes.sessions import sessions as sessions_router
 from app.routes.users import users as users_router
-from app.services import auth
 
 Base.metadata.create_all(engine)
 
