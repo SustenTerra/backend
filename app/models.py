543b3246e19640afcb9867d5fde50cb45fe2360b
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-23 08:37:29 -0300

    feat: join chapters and contents on courses

--- app/models.py
+++ app/models.py
@@ -83,13 +83,16 @@ class Course(Base):
     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
 
     course_chapters: Mapped[List["CourseChapter"]] = relationship(
-        back_populates="course"
+        back_populates="course",
+        lazy="joined",
+        order_by="CourseChapter.index",
     )
     course_category_id: Mapped[int] = mapped_column(
         ForeignKey("course_categories.id")
     )
     course_category: Mapped["CourseCategory"] = relationship(
-        back_populates="courses"
+        back_populates="courses",
+        lazy="joined",
     )
 
 
@@ -119,7 +122,9 @@ class CourseChapter(Base):
     course: Mapped["Course"] = relationship(back_populates="course_chapters")
 
     chapter_contents: Mapped[List["ChapterContent"]] = relationship(
-        back_populates="course_chapter"
+        back_populates="course_chapter",
+        lazy="joined",
+        order_by="ChapterContent.index",
     )
 
 
