5ba45d9e83b39099e080df469cfd5c4f5c696cbf
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-23 09:18:10 -0300

    feat: list courses with less information

--- app/routes/courses.py
+++ app/routes/courses.py
@@ -1,9 +1,11 @@
-from typing import Optional
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends
 
 from app.controllers.course import CourseController
 from app.deps import get_course_controller
+from app.schemas.course import CourseListView
+from app.services.auth import get_logged_user
 
 courses = APIRouter(tags=["courses"])
 
@@ -11,6 +13,7 @@ courses = APIRouter(tags=["courses"])
 @courses.get(
     "/courses",
     description="List all courses",
+    response_model=List[CourseListView],
 )
 def list_all_courses(
     category_name: Optional[str] = None,
@@ -23,6 +26,7 @@ def list_all_courses(
 @courses.get(
     "/courses/{course_id}",
     description="Get one course by id",
+    dependencies=[Depends(get_logged_user)],
 )
 def get_course_by_id(
     course_id: int,
