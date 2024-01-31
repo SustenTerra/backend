e2e453224480e9677c679e9473ce33a60bfddaef
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-01-31 07:51:32 -0300

    feat: list categories endpoint

--- app/server.py
+++ app/server.py
@@ -9,6 +9,9 @@ from app.routes.course_categories import (
     course_categories as course_categories_router,
 )
 from app.routes.courses import courses as courses_router
+from app.routes.post_categories import (
+    post_categories as post_categories_router,
+)
 from app.routes.posts import posts as posts_router
 from app.routes.sessions import sessions as sessions_router
 from app.routes.users import users as users_router
@@ -21,5 +24,6 @@ app.include_router(users_router)
 app.include_router(sessions_router)
 app.include_router(courses_router)
 app.include_router(course_categories_router)
+app.include_router(post_categories_router)
 app.include_router(chapter_contents_router)
 app.include_router(posts_router)
