2e71bd30112abff7168f8561fbbe915b4439df35
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2024-02-14 00:42:02 -0300

    feat: finish chat controller

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -2,15 +2,17 @@ from fastapi import APIRouter, Depends
 
 from app.controllers.chat import ChatController
 from app.deps import get_chat_controller
+from app.models import User
 from app.schemas.chat import ResponseChat, UpdateChat
 from app.services.auth import get_logged_user
 
-chat = APIRouter(tags=["chat"], dependencies=[Depends(get_logged_user)])
+chat = APIRouter(tags=["chat"])
 
 
 @chat.post("/chat", response_model=ResponseChat)
 def update_chat(
     body: UpdateChat,
     chat_controller: ChatController = Depends(get_chat_controller),
+    user: User = Depends(get_logged_user),
 ):
-    return chat_controller.update_chat(body)
+    return chat_controller.update_chat(body, user)
