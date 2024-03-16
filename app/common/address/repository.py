f55a130c7a8c3956865df2e39699194a29f6c3ea
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2024-03-16 19:53:08 -0300

    Fix by CR

--- app/common/address/repository.py
+++ app/common/address/repository.py
@@ -5,8 +5,5 @@ from app.models import Address
 
 
 class AddressRepository(BaseRepository[Address]):
-    def get_check_user_id(self, user_id: int) -> Optional[int]:
-        return self.default_query.filter_by(user_id=user_id).first()
-
     def get_address_by_user_id(self, user_id: int) -> Optional[Address]:
         return self.default_query.filter_by(user_id=user_id).first()
