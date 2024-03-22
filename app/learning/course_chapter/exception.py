14755686ade36738e29b55b07fb7d01d7ef85132
Henrique-Rmc <henriqueribcruz_@hotmail.com>
2024-03-22 08:53:51 -0300

    Fix: update course-chapter name

--- app/learning/course_chapter/exception.py
+++ app/learning/course_chapter/exception.py
@@ -7,3 +7,11 @@ class UserDontMatchCourseOwnerException(HTTPException):
             status_code=403,
             detail="Course Author does not match the current User",
         )
+
+
+class ChapterIdNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=404,
+            detail="Course Chapter Id not Found",
+        )
--- app/learning/course_chapter/route.py
+++ app/learning/course_chapter/route.py
@@ -1,9 +1,10 @@
+from click import edit
 from fastapi import APIRouter, Depends
 
 from app.learning.course.schema import CourseChapterView
 from app.learning.course_chapter.controller import CourseChapterController
 from app.learning.course_chapter.deps import get_course_chapter_controller
-from app.learning.course_chapter.schema import CourseChapterCreate
+from app.learning.course_chapter.schema import CourseChapterCreate, CourseChapterUpdate
 from app.models import User
 from app.service.auth import get_logged_teacher_user
 
@@ -17,7 +18,7 @@ course_chapters = APIRouter(tags=["course_chapters"])
     description="Create course_chapter",
     response_model=CourseChapterView,
 )
-def create_chapter_course(
+def create_course_chapter(
     body: CourseChapterCreate,
     user: User = Depends(get_logged_teacher_user),
     controller: CourseChapterController = Depends(
@@ -25,3 +26,20 @@ def create_chapter_course(
     ),
 ):
     return controller.create(user.id, body)
+
+
+@course_chapters.patch(
+    "/course_chapter/{course_chapter_id}",
+    tags=["course_chapters"],
+    description="Edit course_chapter",
+    response_model=CourseChapterView
+)
+def edit_course_chapter(
+    course_chapter_id: int,
+    body: CourseChapterUpdate,
+    user: User = Depends(get_logged_teacher_user),
+    controller: CourseChapterController = Depends(
+        get_course_chapter_controller
+    ),
+):
+    return controller.update(user.id, course_chapter_id, body )
