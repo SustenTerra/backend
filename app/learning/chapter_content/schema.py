b99a9cb924d326d2893669fb4b9b181b60b00103
marianesc <mariane.carvalho@ccc.ufcg.edu.br>
2024-04-06 10:10:04 -0300

    remove index from schema

--- app/learning/chapter_content/schema.py
+++ app/learning/chapter_content/schema.py
@@ -17,7 +17,6 @@ class ChapterContentCreate(ChapterContentBase):
 
 class ChapterContentUpdate(BaseModel):
     name: Optional[str] = Field(default=None)
-    index: Optional[int] = Field(default=None)
     description: Optional[str] = Field(default=None)
     video_url: Optional[str] = Field(default=None)
 
