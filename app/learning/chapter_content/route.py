b9ca799919c4afd556ff4601fdcc65a6b6819723
marianesc <mariane.carvalho@ccc.ufcg.edu.br>
2024-04-05 22:08:48 -0300

    add content exclusion

--- app/learning/chapter_content/route.py
+++ app/learning/chapter_content/route.py
@@ -4,7 +4,7 @@ from app.learning.chapter_content.controller import ChapterContentController
 from app.learning.chapter_content.deps import get_chapter_content_controller
 from app.learning.chapter_content.schema import ChapterContentView
 from app.models import User
-from app.service.auth import get_logged_user
+from app.service.auth import get_logged_teacher_user, get_logged_user
 
 chapter_contents = APIRouter(
     tags=["chapter_contents"],
@@ -24,3 +24,18 @@ def get_content_by_id(
     ),
 ):
     return controller.get_by_id(chapter_content_id, user.id)
+
+
+@chapter_contents.delete(
+    "/chapter_contents/{chapter_content_id}",
+    description="Delete one chapter_content by id",
+    response_model=None,
+)
+def delete_content(
+    chapter_content_id: int,
+    user: User = Depends(get_logged_teacher_user),
+    controller: ChapterContentController = Depends(
+        get_chapter_content_controller
+    ),
+):
+    controller.delete(chapter_content_id)
