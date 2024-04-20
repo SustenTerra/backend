f303a3396c3a11a3928b9eb5bdeebb91a07a979b
marianesc <mariane.carvalho@ccc.ufcg.edu.br>
2024-04-20 16:02:26 -0300

    add description to schema

--- app/learning/chapter_content/schema.py
+++ app/learning/chapter_content/schema.py
@@ -20,9 +20,13 @@ class ChapterContentCreateWithIndex(ChapterContentBase):
 
 
 class ChapterContentUpdate(BaseModel):
-    name: Optional[str] = Field(default=None)
-    description: Optional[str] = Field(default=None)
-    video_url: Optional[str] = Field(default=None)
+    name: Optional[str] = Field(description="Name of the content", default=None)
+    description: Optional[str] = Field(
+        description="Description of the content", default=None
+    )
+    video_url: Optional[str] = Field(
+        description="URL of the content video", default=None
+    )
 
 
 class ChapterContentView(ChapterContentBase):
